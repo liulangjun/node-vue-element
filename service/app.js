6545f08529fdf18ec84c83bde3c9765f0fb5c2e9
yangyuliang0 <5152731+yangyuliang0@user.noreply.gitee.com>
2020-05-07 03:09:23 +0800

    静态托管

--- service/app.js
+++ service/app.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded())
 
 app.use('/api/user', userApi);
+app.use(express.static('../dist'));
 
 app.listen(3000);
 console.log('success listen at port: 3000')
